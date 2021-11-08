f411cdde0703c696e1666263cd1b95a187cc6eca
sholly111 <sholly2@illinois.edu>
2021-11-07 18:35:44 -0600

    finished class hierarchy

--- Project/SavannahClass.py
+++ Project/SavannahClass.py
@@ -22,7 +22,7 @@ pygame.init
 # Parent class for all bocks
 class Block(pygame.sprite.Sprite):
     def __init__(self, set_x, set_y, set_type, set_is_you, set_can_push, set_is_win, set_is_stop, set_image):
-        super().__init__(Block, self)
+        super(Block, self).__init__()
         # Dimensions in pixels
         self.dimension = (64, 64)
         self.x = set_x
@@ -36,25 +36,45 @@ class Block(pygame.sprite.Sprite):
         self.surf.set_colorkey((255, 255, 255), RLEACCEL)
 
     # Move function
-    def move(self, pressed_keys):
+    def move(self, block_to_move, pressed_keys):
+        if block_to_move.can_push:
         # Move blocks based on key presses
-        if pressed_keys[K_UP]:
-            self.rect.move_ip(0, -5)
-        if pressed_keys[K_DOWN]:
-            self.rect.move_ip(0, 5)
-        if pressed_keys[K_LEFT]:
-            self.rect.move_ip(-5, 0)
-        if pressed_keys[K_RIGHT]:
-            self.rect.move_ip(5, 0)
+            if pressed_keys[K_UP]:
+                self.rect.move_ip(0, -5)
+            if pressed_keys[K_DOWN]:
+                self.rect.move_ip(0, 5)
+            if pressed_keys[K_LEFT]:
+                self.rect.move_ip(-5, 0)
+            if pressed_keys[K_RIGHT]:
+                self.rect.move_ip(5, 0)
 
         # Keep blocks on screen
-        if self.rect.left < 0:
-            self.rect.left = 0
-        if self.rect.right > SCREEN_WIDTH:
-            self.rect.right = SCREEN_WIDTH
-        if self.rect.top <= 0:
-            self.rect.top = 0
-        if self.rect.bottom >= SCREEN_HEIGHT:
-            self.rect.bottom = SCREEN_HEIGHT
+            if self.rect.left < 0:
+                self.rect.left = 0
+            if self.rect.right > SCREEN_WIDTH:
+                self.rect.right = SCREEN_WIDTH
+            if self.rect.top <= 0:
+                self.rect.top = 0
+            if self.rect.bottom >= SCREEN_HEIGHT:
+                self.rect.bottom = SCREEN_HEIGHT
+
+# Object block class
+class Object(Block):
+    def __init__(self, set_x, set_y, set_is_you, set_can_push, set_is_win, set_is_stop, set_image):
+        super().__init__(set_x, set_y, "Object", set_is_you, set_can_push, set_is_win, set_is_stop, set_image)
 
 
+# Variable block class
+class Variable(Block):
+    def __init__(self, set_x, set_y, set_image):
+        super().__init__(set_x, set_y, "Variable", False, True, False, True, set_image)
+
+# Attribute block class
+class Attribute(Block):
+    def __init__(self, set_x, set_y, set_image):
+        super().__init__(set_x, set_y, "Attribute", False, True, False, True, set_image)
+
+# Operator block class
+class Operator(Block):
+    def __init__(self, set_x, set_y, set_image):
+        super().__init__(set_x, set_y, "Operator", False, True, False, True, set_image)
