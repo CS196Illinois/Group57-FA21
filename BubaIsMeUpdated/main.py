4aa9f7fc8c6a20a21f988716eae7085db57e43b4
Akul Datta <35177224+AkulDatta@users.noreply.github.com>
2021-11-22 15:27:19 -0800

    game loop added

--- BubaIsMeUpdated/main.py
+++ BubaIsMeUpdated/main.py
@@ -1,4 +1,4 @@
-# BubaIsYou
+# BubaIsMe
 import pygame
 
 # Import pygame.locals for easier access to key coordinates
@@ -9,9 +9,9 @@ from pygame.locals import (
     K_UP,
     K_DOWN,
     K_LEFT,
-    K_RIGHT#,
+    K_RIGHT,
     #K_ESCAPE,
-    #KEYDOWN,
+    KEYDOWN#,
     #QUIT,
 )
 
@@ -22,9 +22,30 @@ SCREEN_HEIGHT = 640
 # Initialize pygame
 pygame.init()
 
+def move(block_to_move, pressed_keys):
+        if block_to_move.is_push and (not block_to_move.is_stop):
+            if pressed_keys[K_UP]:
+                block_to_move.rect.move_ip(0, -5)
+            if pressed_keys[K_DOWN]:
+                block_to_move.rect.move_ip(0, 5)
+            if pressed_keys[K_LEFT]:
+                block_to_move.rect.move_ip(-5, 0)
+            if pressed_keys[K_RIGHT]:
+                block_to_move.rect.move_ip(5, 0)
+
+            # Keep the block on the screen
+            if block_to_move.rect.left < 0:
+                block_to_move.rect.left = 0
+            if block_to_move.rect.right > SCREEN_WIDTH:  # SCREEN_WIDTH is defined outside the game loop
+                block_to_move.rect.right = SCREEN_WIDTH
+            if block_to_move.rect.top <= 0:
+                block_to_move.rect.top = 0
+            if block_to_move.rect.bottom >= SCREEN_HEIGHT:  # SCREEN_HEIGHT is defined outside the game loop
+                block_to_move.rect.bottom = SCREEN_HEIGHT
 # Parent Block Class
 class Block(pygame.sprite.Sprite):
-    def __init__(self, set_x, set_y, set_type, set_is_you, set_can_push, set_is_win, set_is_stop, set_image):
+    def __init__(self, set_x, set_y, set_type, set_is_you, set_can_push,
+                 set_is_win, set_is_stop, set_image):
         super(Block, self).__init__()
         self.dimension = (64, 64)
         self.x = set_x
@@ -39,48 +60,43 @@ class Block(pygame.sprite.Sprite):
 
     # Move blocks based on pressed_keys
     # pressed_keys is defined in the game loop and is a set of the keys pressed
-    def move(self, block_to_move, pressed_keys):
-        if block_to_move.is_push and (not block_to_move.is_stop):
-            if pressed_keys[K_UP]:
-                self.rect.move_ip(0, -5)
-            if pressed_keys[K_DOWN]:
-                self.rect.move_ip(0, 5)
-            if pressed_keys[K_LEFT]:
-                self.rect.move_ip(-5, 0)
-            if pressed_keys[K_RIGHT]:
-                self.rect.move_ip(5, 0)
-
-            # Keep the block on the screen
-            if self.rect.left < 0:
-                self.rect.left = 0
-            if self.rect.right > SCREEN_WIDTH:       # SCREEN_WIDTH is defined outside the game loop
-                self.rect.right = SCREEN_WIDTH
-            if self.rect.top <= 0:
-                self.rect.top = 0
-            if self.rect.bottom >= SCREEN_HEIGHT:           # SCREEN_HEIGHT is defined outside the game loop
-                self.rect.bottom = SCREEN_HEIGHT
+    
 
 
 # Object class
 class Object(Block):
-    def __init__(self, set_x, set_y, set_is_you, set_can_push, set_is_win, set_is_stop, set_image):
-        super().__init__(set_x, set_y, "Object", set_is_you, set_can_push, set_is_win, set_is_stop, set_image)
+    def __init__(self, set_x, set_y, set_is_you, set_can_push, set_is_win,
+                 set_is_stop, set_image):
+        super().__init__(set_x, set_y, "Object", set_is_you, set_can_push,
+                         set_is_win, set_is_stop, set_image)
 
 
 # Variable Class
 class Variable(Block):
     def __init__(self, set_x, set_y, set_image):
-        super().__init__(set_x, set_y, "Variable", False, True, False, True, set_image)
+        super().__init__(set_x, set_y, "Variable", False, True, False, True,
+                         set_image)
 
 
 # Attribute Class
 class Attribute(Block):
     def __init__(self, set_x, set_y, set_image):
-        super().__init__(set_x, set_y, "Attribute", False, True, False, True, set_image)
+        super().__init__(set_x, set_y, "Attribute", False, True, False, True,
+                         set_image)
 
 
 # Operator Class
 class Operator(Block):
     def __init__(self, set_x, set_y, set_image):
-        super().__init__(set_x, set_y, "Operator", False, True, False, True, set_image)
+        super().__init__(set_x, set_y, "Operator", False, True, False, True,
+                         set_image)
+
+has_won = False
+block_array = []
+while (not has_won):
+	for event in pygame.event.get():
+		if (event.type == KEYDOWN):
+			for i in range(block_array):
+				if (block_array[i].can_move == True):
+					move(block_array[i])
 
